ecc60517361c37a2c2df2c8b828e6d140675e96b
Sayyed <Wasim.Sayyed@emc.com>
2020-07-26 13:56:05 +0530

    server files added

---
+++ server/models/index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const db = {};
+
+db.mongoose = mongoose;
+
+db.user = require("./user.model");
+db.role = require("./role.model");
+db.product = require("./product.model");
+db.category = require("./category.model");
+
+db.ROLES = ["user", "admin", "moderator"];
+
+module.exports = db;
---
+++ server/server.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dbConfig = require("./config/db.config");
+
+const app = express();
+
+
+const products = require('./routes/product.routes');
+const categories = require('./routes/categories.routes');
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse requests of content-type - application/json
+//app.use(bodyParser.json());
+app.use(express.json());
+
+const db = require("./models");
+const Role = db.role;
+
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+// require('./routes/product.routes')(app);
+// require('./routes/categories.routes')(app);
+
+const seeder = require('./routes/seeder/products');
+
+app.use('/seeder', seeder);
+
+app.use('/products', products);
+app.use('/categories', categories);
+
+
+db.mongoose
+    .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connect to MongoDB.");
+        initial();
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            new Role({
+                name: "user"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'user' to roles collection");
+            });
+
+            new Role({
+                name: "moderator"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'moderator' to roles collection");
+            });
+
+            new Role({
+                name: "admin"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+
+                console.log("added 'admin' to roles collection");
+            });
+        }
+    });
+}
+
+
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to shopping application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
